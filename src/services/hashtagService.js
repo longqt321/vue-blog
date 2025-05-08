8f3060e0b33926f3af3e4a3992893adffbf04711
longqt321 <longqt321@gmail.com>
2025-05-08 09:37:04 +0700

    Updated: hashtags sorted desc by postCount

--- src/services/hashtagService.js
+++ src/services/hashtagService.js
@@ -4,7 +4,7 @@ export default {
   async getPopularHashtags() {
     try {
       const response = await api.get(`hashtags/popular`);
-      return response.data;
+      return response.data.content.map((h) => h.name);
     } catch (error) {
       console.error("ERROR FECTH POPULAR HASHTAGS", error);
     }
